171b1d5f7667aa7908b170a56ad7714575303bce
dergunovalexey <dergunovalexey@mail.ru>
2018-05-02 16:41:06 +0700

    add view, template and url

--- sci_guide/urls.py
+++ sci_guide/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import get_main_info
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_main_info),
 ]
